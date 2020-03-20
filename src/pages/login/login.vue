b75c9c40efc458a8b514a9245e23551d0ce8d8b2
hero7212 <2423467330@qq.com>
2020-03-20 11:03:34 +0800

    登录页样式v1

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -5,7 +5,15 @@
 				安全培训系统
 			</view>
 			<view class="row">
-				
+				<uni-icons type="contact" size="30" class="icon"></uni-icons>
+				<input type="text" v-model="form.account" placeholder="请输入工号" class="input"/>
+			</view>
+			<view class="row">
+				<uni-icons type="eye-slash" size="30" class="icon"></uni-icons>
+				<input type="password" v-model="form.password" placeholder="请输入密码" class="input"/>
+			</view>
+			<view class="btn">
+				 <button type="primary" block @click="submit">登录</button>
 			</view>
 		</view>
 		
@@ -18,8 +26,28 @@
 		components: {uniIcons},
 		data() {
 			return {
+				form: {
+					account: '',
+					password: ''
+				},
 				
 			};
+		},
+		methods: {
+			submit() {
+				let {account, password} = this.form
+				
+				if ([account, password].includes('')) {
+					uni.showToast({
+						icon: 'none',
+						title: '工号或密码不正确',
+						duration: 1000
+					})
+				}
+			}
+		},
+		mounted() {
+			
 		}
 	}
 </script>
@@ -28,17 +56,43 @@
 @import '../../uni.styl';
 .login {
 	color: #ffffff;
+	background: url(https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1584672785&di=73c510559b4f28b5ee5f01c7a7418e94&src=http://img.jk51.com/img_jk51/330795344.jpeg) center center no-repeat;
+	background-size: cover; 
 	.title {
 		font-size: 64upx;
-		padding: 40upx 0;
+		padding: 60upx 0 20vh;
+		text-shadow: 0 0 20upx 20upx $uni-text-color;
 	}
 	.row {
 		width: 80%;
-		height: 40upx;
-		border-radius: 40upx;
-		margin: 20upx auto;
+		height: 80upx;
+		border-radius: 80upx;
+		margin: 40upx auto;
 		background: #ffffff;
 		position: relative;
+		.icon {
+			position: absolute;
+			top: 10upx;
+			left: 10upx;
+		}
+		.input {
+			display: block;
+			height: 80upx;
+			position absolute;
+			top: 0;
+			left: 80upx;
+			right: 20upx;
+			bottom: 0;
+			font-size: 36upx;
+			color: $uni-text-color-muted;
+		}
+	}
+	.btn {
+		width: 80%;
+		margin: 120upx auto 0;
+		.login-btn {
+			background: uni-bg-color-primary;
+		}
 	}
 }
 </style>
